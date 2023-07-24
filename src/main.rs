ecd23be644fcecdc33c751b4d31ba8c64a86ee47
Jack Stanton <stantonjack3@gmail.com>
2023-07-24 14:56:56 +1000

    helpfile

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io::Write;
+use std::process::Command;
 use std::io::stdout;
 use std::io::stdin;
+use std::fs;
+use std::process;
 
 fn main(){
     loop {
@@ -11,12 +14,30 @@ fn main(){
         stdin().read_line(&mut command)
             .expect("Failed to read in command");
 
-        let command_split : Vec<&str> = command.replace("\n", "").split(' ').collect();
+        command = command.replace("\n",""); // replace new line character in command, just cause
+
+        let command_split : Vec<&str> = command.split(' ').collect();
 
         let keyword = command_split[0];
         let args = &command_split[1..];
 
-        println!("{:?}", command_split);
+        match keyword{
+            "help" => {
+                let helpfile = fs::read_to_string("src/HELPFILE").expect("Failed to read the file.");
+                println!("{}",helpfile);
+            }
+            "end" => {
+                println!("exiting with code 0x0100");
+                process::exit(0x0100);
+            }
+            keyword => {
+                let mut child = Command::new(keyword)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
+
+                child.wait();
+            }
+        }
     }
 }
-
