63ebaeb9d34b2272e024fdaddefee594424e7e4d
Jack Stanton <stantonjack3@gmail.com>
2023-09-22 17:43:16 +1000

    removing async from download

--- src/download.rs
+++ src/download.rs
@@ -3,17 +3,17 @@ use std::io::Cursor;
 use crate::setToHomeDir;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
  
-async fn downloadToFile(url: String, file_name: String) -> Result<()> {
-    let response = reqwest::get(url).await?;
+fn downloadToFile(url: String, file_name: String) -> Result<()> {
+    let response = reqwest::blocking::get(url)?;
     let mut file = std::fs::File::create(file_name)?;
-    let mut content =  Cursor::new(response.bytes().await?);
+    let mut content =  Cursor::new(response.bytes()?);
     std::io::copy(&mut content, &mut file)?;
     Ok(())
 }
  
-pub async fn update(ver: &str) {
+pub fn update(ver: &str) {
     setToHomeDir(true, ".gsh/bin");
     let binaryurl = "https://github.com/jaevibing/g-shell/releases/download/".to_string() + ver + "/gsh";
-    downloadToFile(binaryurl, "gsh".to_string()).await.unwrap();
+    downloadToFile(binaryurl, "gsh".to_string()).unwrap();
     println!("New binary downloaded, restarting...");
 }
