f8538ea44b281a8a9eaba2e6a0f671a66fa3ed1d
Jack Stanton <stantonjack3@gmail.com>
2023-09-22 12:10:22 +1000

    better cursor movement

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ mod autoupdate;
 mod download;
 mod history;
 mod saveLastCheck;
-mod keyFunctions;
 
-use termion::raw::IntoRawMode;
+use termion::{raw::IntoRawMode, input::TermRead, async_stdin, event::Key};
 use tokio::runtime::Runtime;
 use colored::Colorize;
-use std::{env, io::{Write, stdout, stdin}, path::Path, process::{Command, self}, time::{SystemTime, UNIX_EPOCH}, thread, sync::{Arc, Mutex}};
+use std::{env, io::{Write, stdout, stdin}, path::Path, process::{Command, self}, time::{SystemTime, UNIX_EPOCH}};
 
 pub fn setToHomeDir(gsh: bool, pathFromHome: &str){
     #[allow(deprecated)]
@@ -30,9 +29,8 @@ pub fn setToHomeDir(gsh: bool, pathFromHome: &str){
 }
 
 fn main(){
-    //let mut termout_lock = std::io::stdout().into_raw_mode().unwrap();
-    let termout = Arc::new(Mutex::new(std::io::stdout().into_raw_mode().unwrap()));
-    let mut termout_lock = termout.lock().unwrap();
+    //let mut termout = std::io::stdout().into_raw_mode().unwrap();
+    let mut termout = std::io::stdout().into_raw_mode().unwrap();
 
     let version = include_str!("docs/VERSION");
     // let version = "v0.0.3"; // debug version number for testing autoupdate
@@ -46,19 +44,19 @@ fn main(){
     drop(rt);
 
     if gitversion != version {
-        writeln!(termout_lock, "You are not currently running the latest version of g-shell.");
-        writeln!(termout_lock, "{} -> {}", version.red(), gitversion.green());
-        writeln!(termout_lock, "Do you wish to update? (Y/n)");
+        writeln!(termout, "You are not currently running the latest version of g-shell.");
+        writeln!(termout, "{} -> {}", version.red(), gitversion.green());
+        writeln!(termout, "Do you wish to update? (Y/n)");
         let mut choice = String::new();
         stdin().read_line(&mut choice)
             .expect("Could not read input command.");
-        write!(termout_lock, "{choice}");
+        write!(termout, "{choice}");
         if choice.contains("Y") {
             setToHomeDir(true, ".gsh");
             download::update(gitversion.as_str());
         }
         else {
-            writeln!(termout_lock, "Avoiding update, you will be prompted again on next startup.\nYou can update in the terminal with the gsh-update command.\n");
+            writeln!(termout, "Avoiding update, you will be prompted again on next startup.\nYou can update in the terminal with the gsh-update command.\n");
             let value = gitversion.clone() + ":" + &SystemTime::now().duration_since(UNIX_EPOCH)
                                                     .expect("oopsie poopsie a friggin time error beitch")
                                                     .as_secs()
@@ -66,34 +64,74 @@ fn main(){
             let _ = saveLastCheck::saveLastCheck(value);
         }
     }
-
-    thread::spawn({
-        let termout = Arc::clone(&termout); // share termout with thread
-        move || {
-            keyFunctions::getKeyPresses(termout);
-        }
-    });
     
     loop {
         let current_dir = env::current_dir()
             .unwrap()
             .to_string_lossy()
             .to_string();
-        write!(termout_lock, "gsh [{}] $ ", current_dir);
+        write!(termout, "gsh [{}] $ ", current_dir);
         let _ = stdout().flush();
 
+        let mut fuck = false;
+        let mut cursor_pos = 0;
         let mut command = String::new();
-        stdin().read_line(&mut command)
-            .expect("Could not read input command.");
+
+        while !fuck {
+            if let Some(Ok(key_event)) = stdin().keys().next() {
+                match key_event {
+                    Key::Char('\n') => {
+                        write!(termout, "\n");
+                        fuck = true;
+                        break;
+                    }
+                    Key::Left => {
+                        if cursor_pos == 0 {
+                            write!(termout, "\x07");
+                        }
+                        else {
+                            write!(termout, "\x1b[D");
+                        }
+                        cursor_pos -= 1;
+                    }
+                    Key::Right => {
+                        if cursor_pos == command.as_str().len() {
+                            write!(termout, "\x07");
+                        }
+                        else {
+                            write!(termout, "\x1b[C");
+                        }
+                        cursor_pos += 1;
+                    }
+                    Key::Up => {
+                        write!(termout, "\x07");
+                    }
+                    Key::Down => {
+                        write!(termout, "\x07");
+                    }
+                    Key::Char(n) => {
+                        command.push_str(n.to_string().as_str());
+                        cursor_pos += 1;
+                        write!(termout, "{}", n);
+                    }
+                    _ => todo!(),
+                }
+            }
+    
+            // You can add your rendering or game logic here.
+    
+            // Be sure to flush the stdout to ensure changes are visible.
+            stdout().flush();
+        }
         
         /* 
         get ready for some high quality autism in 
         the form of ansi text commands
         */
 
-        write!(termout_lock, "\u{1b}[1;A"); // move cursor back to beginning of output
-        write!(termout_lock, "\r\x1b[K"); // delete output 
-        write!(termout_lock, "$ {command}"); // reprint command without bells and whistles
+        write!(termout, "\u{1b}[1;A"); // move cursor back to beginning of output
+        write!(termout, "\r\x1b[K"); // delete output 
+        write!(termout, "$ {command}"); // reprint command without bells and whistles
 
         let _ = history::writeToHistory(&command);
 
@@ -114,10 +152,10 @@ fn main(){
                             .to_string_lossy()
                             .into_owned();
                         drop(current_dir);
-                        writeln!(termout_lock, "{}", current_dir_str);
+                        writeln!(termout, "{}", current_dir_str);
                     }
                     Err(e) => {
-                        writeln!(termout_lock, "error getting working directory: {}", e);
+                        writeln!(termout, "error getting working directory: {}", e);
                     }
                 }
             },
@@ -125,10 +163,10 @@ fn main(){
                 let helpfile = include_str!("docs/HELPFILE");
                 let helpheader = "--- g-shell ".to_string() + version + " pre-alpha ---";
                 let helpend = "--- end help ---\n";
-                writeln!(termout_lock, "\n{helpheader}\n\n{helpfile}\n\n{helpend}");
+                writeln!(termout, "\n{helpheader}\n\n{helpfile}\n\n{helpend}");
             },
             "exit" => {
-                writeln!(termout_lock, "exiting with code 0x0100");
+                writeln!(termout, "exiting with code 0x0100");
                 process::exit(0x0100);
             },
             #[allow(deprecated)]
@@ -141,7 +179,7 @@ fn main(){
             },
             "gsh-update" => {
                 if gitversion == version{
-                    writeln!(termout_lock, "You are already on the latest version of g-shell.\n");
+                    writeln!(termout, "You are already on the latest version of g-shell.\n");
                 }
                 else{
                     setToHomeDir(true, ".gsh");
@@ -149,7 +187,7 @@ fn main(){
                 }
             },
             "gsh-info" => {
-                write!(termout_lock, "gsh pre-alpha {}\n", version);
+                write!(termout, "gsh pre-alpha {}\n", version);
             },
             keyword => {
                 let mut child = Command::new(keyword)
